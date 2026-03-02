9c29d5700ea79ab086a12c7d80b274fcbc3926ff
M <2480177@cognizant.com>
2026-03-02 10:02:26 +0530

    feat: Added contract, vendor, delivery and invoice

---
+++ src/main/java/com/cts/eventsphere/model/data/InvoiceStatus.java
@@ -0,0 +1,13 @@
+package com.cts.eventsphere.model.data;
+
+/**
+ * Indicates the financial processing status of an invoice.
+ *
+ * @author 2480177
+ * @version 1.0
+ * @since 26-02-2026
+ */
+
+public enum InvoiceStatus {
+    issued, paid, overdue, cancelled
+}
---
+++ src/main/java/com/cts/eventsphere/repository/InvoiceRepository.java
@@ -0,0 +1,17 @@
+package com.cts.eventsphere.repository;
+
+import com.cts.eventsphere.model.Invoice;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Repository for persisting and querying invoice entities.
+ *
+ * @author 2480177
+ * @version 1.0
+ * @since 26-02-2026
+ */
+
+@Repository
+public interface InvoiceRepository extends JpaRepository<Invoice, String> {
+}
