eaedf83cd4a44bb3f88859522bfc130aed22488e
Shahid <2480081@cognizant.com>
2026-03-09 22:40:09 +0530

    Adding Validation and global exception handler to handle errors for the validation along with added security to my controllers

--- src/main/java/com/cts/eventsphere/dto/expense/ExpenseRequestDto.java
+++ src/main/java/com/cts/eventsphere/dto/expense/ExpenseRequestDto.java
@@ -1,5 +1,10 @@
 package com.cts.eventsphere.dto.expense;
 
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -13,7 +18,14 @@ import java.util.Date;
  * @since 01-03-2026
  */
 public record ExpenseRequestDto(
+        @NotBlank(message = "Description cannot be empty")
+        @Size(max = 255 , message = "Description provided is too long")
         String description,
+
+        @NotNull(message = "Amount is required")
+        @DecimalMin(value = "0.0",inclusive = false,message = "Amount must be greater than 0")
         BigDecimal amount,
+
+        @NotNull(message = "Date is required")
         LocalDate date
 ) { }
