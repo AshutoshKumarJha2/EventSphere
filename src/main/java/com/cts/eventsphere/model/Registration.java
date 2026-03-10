01ca1eec6be3737d9babd06f95321cd9627211ab
test-in-prod-10x <kaushik.chowdhury@cognizant.com>
2026-03-10 12:44:00 +0530

    feat: Fixed controllers

--- src/main/java/com/cts/eventsphere/model/Registration.java
+++ src/main/java/com/cts/eventsphere/model/Registration.java
@@ -3,9 +3,7 @@ package com.cts.eventsphere.model;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
-import org.hibernate.annotations.UuidGenerator;
+import org.hibernate.annotations.*;
 
 import com.cts.eventsphere.model.data.RegistrationStatus;
 
@@ -31,6 +29,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 @Entity
+@DynamicInsert
 public class Registration {
     @Id
     @UuidGenerator
